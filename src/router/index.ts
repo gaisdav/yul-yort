6c795739e856848d2a27973423972076f27f86b7
GDavletkildin <gdavletkildin@beeline.ru>
2021-10-11 10:46:04 +0500

    написан метод для инициализации приложения добавлена страница ошибки при инициализации

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,11 @@ import browserPlugin from "router5-plugin-browser";
 import routes from "./routes";
 import { onActivate } from "./middlewaries/onActivate";
 import { IDependencies } from "./types";
+import { Router } from "router5/dist/types/router";
 
-export default function createAppRouter(dependencies: IDependencies) {
+export default function createAppRouter(
+  dependencies: IDependencies
+): Router<IDependencies> {
   const router = createRouter<IDependencies>(
     routes,
     {
