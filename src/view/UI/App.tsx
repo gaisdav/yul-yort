7930234cb5acb7549164e5bfa3f237878a722121
GDavletkildin <gdavletkildin@beeline.ru>
2021-10-11 15:35:41 +0500

    Начат функционал по показу плашки с уведомлением отключения интернета

--- src/view/UI/App.tsx
+++ src/view/UI/App.tsx
@@ -1,4 +1,4 @@
-import { FC, lazy, Suspense } from "react";
+import { FC, lazy, Suspense, useEffect, useState } from "react";
 import { useRoute } from "react-router5";
 import { constants } from "router5";
 import { LoadingScreen } from "./components/LoadingScreen";
@@ -9,6 +9,17 @@ const NotFoundPage = lazy(() => import("./pages/notFound"));
 
 export const App: FC = () => {
   const router = useRoute();
+  const [online, setOnline] = useState<boolean>(navigator.onLine);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      navigator.onLine !== online && setOnline(navigator.onLine);
+    }, 10000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [online]);
 
   let page: JSX.Element;
 
@@ -26,7 +37,12 @@ export const App: FC = () => {
       page = <NotFoundPage />;
   }
 
-  return <Suspense fallback={<LoadingScreen />}>{page}</Suspense>;
+  return (
+    <Suspense fallback={<LoadingScreen />}>
+      <div>{!online && "Отсутствует соединение с интернетом"}</div>
+      {page}
+    </Suspense>
+  );
 };
 
 export default App;
