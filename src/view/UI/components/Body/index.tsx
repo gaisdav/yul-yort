ad2a3fc8cfe375c36e4d218aa24db73127b464b9
gaisdav <gais.davletkildin@gmail.com>
2022-09-11 16:29:14 +0500

    get css variables from mui theme

--- src/view/UI/components/Body/index.tsx
+++ src/view/UI/components/Body/index.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
 import css from "./styles.module.scss";
+import { useViewModel } from "../../hooks/useViewModel";
+import { IAppVM } from "../../../viewModels/App/types";
 
 const Body: FC = ({ children }) => {
-  return <div className={css.body}>{children}</div>;
+  const appVM = useViewModel<IAppVM>("app");
+
+  return (
+    <div className={css.body} data-theme={appVM.theme}>
+      {children}
+    </div>
+  );
 };
 
 export default Body;
