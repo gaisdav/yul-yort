203e5b6de796dab2b7c003bde7e6949e3eafdc83
afdavletkulov <afdavletkulov@rttv.ru>
2023-11-02 13:35:40 -0300

    fix

--- src/constants/globalConstants.ts
+++ src/constants/globalConstants.ts
@@ -8,5 +8,5 @@ export const CONSTANTS = {
   numberPattern: /^[1-9]\d*(\d+)?$/i,
   publicUrl: process.env.PUBLIC_URL || "/",
   devBaseUrl: "http://localhost:9000/",
-  prodBaseUrl: "https://api.yul-yort",
+  prodBaseUrl: "https://api.yul-yort.ru",
 };
