e89307fd1d2fe39198eaa0644a9ce0f9a3593565
g.davletkildin <gais.davletkildin@gmail.com>
2024-02-23 13:44:12 +0100

    fix themes

--- src/view/UI/hooks/useChangeTheme.tsx
+++ src/view/UI/hooks/useChangeTheme.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useViewModel } from "./useViewModel.tsx";
 import { IAppVM, TTheme } from "../../viewModels/App/types.ts";
 
@@ -12,40 +12,54 @@ export interface IChangeTheme {
  */
 export const useChangeTheme = (): IChangeTheme => {
   const { theme, setTheme } = useViewModel("app");
+  const [bg, setBg] = useState("");
+  const htmlRef = useRef<HTMLElement | null>(null);
   const headerRef = useRef<HTMLElement | null>(null);
+  const themeColorMetaRef = useRef<HTMLElement | null>(null);
+  const bodyRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     // #app-bar это id элемента <AppBar />
     headerRef.current = document.querySelector("#app-bar");
-  }, []);
-
-  // Изменение цвета статус бара в зависимости от темы
-  useEffect(() => {
-    const themeColorMetaTag = document.querySelector(
+    htmlRef.current = document.querySelector("html");
+    bodyRef.current = document.querySelector("body");
+    themeColorMetaRef.current = document.querySelector(
       "meta[name='theme-color']",
     );
-    const bodyTag = document.querySelector("body");
 
-    if (!themeColorMetaTag || !headerRef.current || !bodyTag) {
+    if (!headerRef.current) {
       return;
     }
 
     const headerStyles = getComputedStyle(headerRef.current);
-    const backgroundColor = headerStyles.backgroundColor;
-
-    themeColorMetaTag.setAttribute("content", backgroundColor);
-    bodyTag.style.backgroundColor = backgroundColor;
+    setBg(headerStyles.backgroundColor);
   }, [theme]);
 
-  // Изменение темы приложения через тег <html>
+  // Изменение цвета статус бара в зависимости от темы для Android
   useEffect(() => {
-    const htmlTag = document.querySelector("html");
-
-    if (!htmlTag) {
+    if (!themeColorMetaRef.current || !bg) {
       return;
     }
 
-    htmlTag.setAttribute("data-theme", theme);
+    themeColorMetaRef.current.setAttribute("content", bg);
+  }, [bg]);
+
+  // Изменение цвета статус бара в зависимости от темы для iOS
+  useEffect(() => {
+    if (!bodyRef.current || !bg) {
+      return;
+    }
+
+    bodyRef.current.style.backgroundColor = bg;
+  }, [bg]);
+
+  // Изменение темы приложения через тег <html>
+  useEffect(() => {
+    if (!htmlRef.current) {
+      return;
+    }
+
+    htmlRef.current.setAttribute("data-theme", theme);
   }, [theme]);
 
   return { theme, setTheme };
